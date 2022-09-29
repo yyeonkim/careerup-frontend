efa0dde57c8b45c3d131cae7798488f7c28970ce
Yongyeon Kim <yeonlotus@ewhain.net>
2022-09-29 23:34:48 +0900

    Style: Global style 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Global, css } from '@emotion/react';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        html {
+          font-size: 10px;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>
 );
