6c866dfb0a6b1ce09c93333064f7841c900ce47f
Yongyeon Kim <yeonlotus@ewhain.net>
2022-09-29 23:29:22 +0900

    Fix: eslint와 prettier 충돌 해결
    eslintrc.js에서 prettier rules 삭제

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,17 @@
 // .eslintrc.js
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    plugins: ['prettier', '@typescript-eslint'],
-    rules: {
-        'prettier/prettier': [
-            'error',
-            {
-                singleQuote: true,
-                semi: true,
-                useTabs: false,
-                tabWidth: 2,
-                printWidth: 80,
-                bracketSpacing: true,
-                arrowParens: 'avoid',
-            },
-        ],
-    },
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-    },
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
 };
