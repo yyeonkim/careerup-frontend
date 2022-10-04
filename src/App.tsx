a0e5cf9d0624b6153d944dbbccba401f1d37b3cf
Yongyeon Kim <yeonlotus@ewhain.net>
2022-10-05 01:05:41 +0900

    Test: Home 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
-  return <div>test!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
