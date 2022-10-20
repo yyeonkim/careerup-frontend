780fde76ca240535b5a12e08e21e7fd0c9698a9b
Yongyeon Kim <yeonlotus@ewhain.net>
2022-10-20 18:03:30 +0900

    Feat: 로그인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
       </Switch>
     </Router>
