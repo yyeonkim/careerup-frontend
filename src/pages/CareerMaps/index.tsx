e7953d9b92bb02e927864fb628cd4ccbfd448d56
Yongyeon Kim <yeonlotus@ewhain.net>
2022-11-06 12:06:57 +0900

    Feat: 메인 프로필 메뉴 생성

--- src/pages/CareerMaps/index.tsx
+++ src/pages/CareerMaps/index.tsx
@@ -1,3 +1,22 @@
+import { useState } from 'react';
+import { CareerMapList, Map, MapsContainer, Menu, ProfileContainer, ProfileImg, Title } from './style';
+
 export default function CareerMaps() {
-  return <h1>Maps</h1>;
+  const [careerMaps, setCareerMaps] = useState<number[]>([0, 1, 2]);
+
+  return (
+    <Menu>
+      <ProfileContainer>
+        <ProfileImg src="https://notioly.com/wp-content/uploads/2022/11/181.Nodes_.png" />
+      </ProfileContainer>
+      <CareerMapList>
+        <Title>내 커리어 맵</Title>
+        <MapsContainer>
+          {careerMaps.map((item) => (
+            <Map></Map>
+          ))}
+        </MapsContainer>
+      </CareerMapList>
+    </Menu>
+  );
 }
---
+++ src/pages/CareerMaps/style.ts
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+
+export const Menu = styled.div`
+  padding: 2rem;
+  padding-top: 4rem;
+
+  width: 32rem;
+  height: 100vh;
+  background-color: gray;
+`;
+
+export const ProfileContainer = styled.div`
+  padding-bottom: 4rem;
+`;
+
+export const ProfileImg = styled.img`
+  width: 14rem;
+  border-radius: 50%;
+`;
+
+export const CareerMapList = styled.div`
+  padding: 2rem;
+  padding-bottom: 0;
+
+  border-radius: 2rem;
+  background-color: lightgray;
+`;
+
+export const Title = styled.div`
+  margin-bottom: 2rem;
+
+  font-size: 1.6rem;
+  font-weight: bold;
+`;
+
+export const MapsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+export const Map = styled.div`
+  margin-bottom: 2rem;
+
+  width: 13rem;
+  height: 15rem;
+  border-radius: 2rem;
+  background-color: gray;
+`;
