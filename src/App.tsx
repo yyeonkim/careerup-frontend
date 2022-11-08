66cf2806e3540d5af0925cec0ae73a56676979f8
5hyun <qhslsl@gmail.com>
2022-11-08 20:36:02 +0900

    Feat: 로드맵 임시 주소 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CareerMaps from './pages/CareerMaps';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import RoadMap from './components/RoadMap';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/roadmap">
+          <RoadMap />
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
