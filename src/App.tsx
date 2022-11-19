e89c047a1d1cc1c4d44410f17a7bdc1285c27fe6
5hyun <qhslsl@gmail.com>
2022-11-19 16:34:23 +0900

    Chore: roadmap Route 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import CareerMaps from './pages/CareerMaps';
 import Home from './pages/Home';
-import RoadMap from './components/RoadMap';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/roadmap">
-          <RoadMap />
-        </Route>
         <Route path="/career-maps">
           <CareerMaps />
         </Route>
