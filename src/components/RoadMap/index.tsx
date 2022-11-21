4a4169675fc08f3f9141779b55b14babb8b27ac6
5hyun <qhslsl@gmail.com>
2022-11-21 15:21:23 +0900

    Feat: 길 추가는 구현

--- src/components/RoadMap/index.tsx
+++ src/components/RoadMap/index.tsx
@@ -1,6 +1,50 @@
-import React from 'react';
-import { Wrapper } from './styles';
+import React, { useCallback, useState } from 'react';
+import { RoadMapContainer, MapWrapper, SideBar, AddRoadBtn, Map, FirstRoad, AddRound } from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+const Round = () => {
+  return (
+    <AddRound>
+      <FontAwesomeIcon icon={faPlus} />
+    </AddRound>
+  );
+};
 
 export default function RoadMap() {
-  return <Wrapper>안녕</Wrapper>;
+  const [lenRoad, setLenRoad] = useState(0);
+
+  const onClickAddRoad = useCallback(() => {
+    setLenRoad((prev) => prev + 1);
+  }, []);
+
+  return (
+    <RoadMapContainer>
+      <MapWrapper>
+        <Map>
+          <FirstRoad>
+            <div className={'first'}>
+              <img src="/images/firstMap.png" alt="firstMap" />
+            </div>
+            {[...Array(lenRoad)].map((v, road) => {
+              const roadImg = road % 2 === 0 ? '/images/SecondMap.png' : '/images/ThirdMap.png';
+
+              return (
+                <div className={road % 2 === 0 ? (road > 1 ? 'second2' : 'second') : 'third'}>
+                  <img src={roadImg} alt="Map" />
+                </div>
+              );
+            })}
+            <Round />
+          </FirstRoad>
+        </Map>
+      </MapWrapper>
+      <SideBar>
+        <AddRoadBtn onClick={onClickAddRoad}>
+          길<br />
+          <br /> 추<br />가<br />하<br />기
+        </AddRoadBtn>
+      </SideBar>
+    </RoadMapContainer>
+  );
 }
--- src/components/RoadMap/styles.tsx
+++ src/components/RoadMap/styles.tsx
@@ -1,39 +1,120 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
+export const RoadMapContainer = styled.div`
   font-size: 2rem;
-  border: 1px solid;
-  height: 100vh;
-  width: 100vw;
+  width: 100%;
 
+  background-color: #eeeeee;
+
+  display: grid;
+  grid-template-columns: 95fr minmax(60px, 5fr);
+`;
+
+export const MapWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Map = styled.div`
   position: relative;
 
-  & div {
-    width: 200px;
-    height: 200px;
-    padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-width: 95%;
 
-    border: 1px solid;
+  margin: 5rem 5rem;
+  background-color: #ffffff;
+  border-radius: 5rem;
 
-    & > span,
-    .addBtn {
-      width: 100%;
-      height: 100%;
+  padding: 6rem 0;
 
-      border: 1px solid;
-      border-radius: 50%;
+  & > img {
+    width: 80%;
+  }
+`;
 
-      display: flex;
-      justify-content: center;
-      align-items: center;
+export const FirstRoad = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .first,
+  .second,
+  .third,
+  .second2 {
+    text-align: center;
+
+    & img {
+      width: 80%;
+      height: 95%;
     }
   }
 
-  .table {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
+  .second {
+    margin-top: -8.21%;
+    margin-left: 6.1%;
 
-    width: 100%;
-    border: none;
+    img {
+      width: 78%;
+    }
   }
+
+  .second2 {
+    margin-top: -7.6%;
+    margin-left: 6.1%;
+
+    img {
+      width: 78%;
+    }
+  }
+
+  .third {
+    margin-top: -7.5%;
+    margin-right: 6%;
+
+    img {
+      width: 78%;
+    }
+  }
+`;
+
+export const SideBar = styled.div`
+  display: flex;
+  justify-content: center;
+
+  background-color: #b8b8b8;
+`;
+
+export const AddRoadBtn = styled.button`
+  margin-top: 5rem;
+  width: 4rem;
+  height: 12.3rem;
+
+  border: none;
+  border-radius: 5rem;
+
+  color: #b8b8b8;
+  background-color: #ffffff;
+  font-weight: bold;
+`;
+
+export const AddRound = styled.div`
+  border: 8px solid #85cca3;
+  border-radius: 50%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 4rem;
+  color: #c4c4c4;
+  background-color: #ffffff;
+
+  width: 10%;
+  height: 10%;
+
+  position: absolute;
+  z-index: 999;
+  top: 43px;
 `;
