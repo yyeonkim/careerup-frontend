c4cb3cc37c5d2ad82079c74bb5ceb31bf86c991b
5hyun <qhslsl@gmail.com>
2022-12-17 01:05:28 +0900

    Feat: roadmap 데이터 추가

--- src/reducers/RoadMapSlice.ts
+++ src/reducers/RoadMapSlice.ts
@@ -3,10 +3,16 @@ import { RootState } from '../redux/store';
 
 export interface RoadMapState {
   orderEdit: boolean;
+  roadLen: number;
+  activity: number;
+  isModal: boolean;
 }
 
 const initialState: RoadMapState = {
   orderEdit: false,
+  roadLen: 0,
+  activity: 9,
+  isModal: false,
 };
 
 export const roadMapSlice = createSlice({
@@ -16,13 +22,20 @@ export const roadMapSlice = createSlice({
     toggleOrderEdit: (state) => {
       state.orderEdit = !state.orderEdit;
     },
+    addRoad: (state) => {
+      state.roadLen++;
+      state.activity += 3;
+    },
+    toggleIsModal: (state) => {
+      state.isModal = !state.isModal;
+    },
   },
   extraReducers: (builder) => {
     true;
   },
 });
 
-export const { toggleOrderEdit } = roadMapSlice.actions;
+export const { toggleOrderEdit, addRoad, toggleIsModal } = roadMapSlice.actions;
 export const roadMap = (state: RootState) => state.roadMap;
 
 export default roadMapSlice.reducer;
