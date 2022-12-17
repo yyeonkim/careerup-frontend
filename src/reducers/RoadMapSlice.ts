3a53312991b3428e6d4ef3cdd3b33ea099c862e9
5hyun <qhslsl@gmail.com>
2022-12-18 01:19:58 +0900

    Feat: roadmap 데이터 추가

--- src/reducers/RoadMapSlice.ts
+++ src/reducers/RoadMapSlice.ts
@@ -6,13 +6,33 @@ export interface RoadMapState {
   roadLen: number;
   activity: number;
   isModal: boolean;
+  isActivityTypeModal: boolean;
+  isToolModal: boolean;
+  modalData: {
+    Date: {
+      startYear: number;
+      startMonth: number;
+      endYear: number;
+      endMonth: number;
+    };
+  };
 }
 
 const initialState: RoadMapState = {
   orderEdit: false,
   roadLen: 0,
   activity: 9,
-  isModal: false,
+  isModal: true,
+  isActivityTypeModal: false,
+  isToolModal: false,
+  modalData: {
+    Date: {
+      startYear: new Date().getFullYear(),
+      startMonth: new Date().getMonth() - 1,
+      endYear: new Date().getFullYear(),
+      endMonth: new Date().getMonth(),
+    },
+  },
 };
 
 export const roadMapSlice = createSlice({
@@ -28,6 +48,34 @@ export const roadMapSlice = createSlice({
     },
     toggleIsModal: (state) => {
       state.isModal = !state.isModal;
+      if (!state.isModal) {
+        state.isToolModal = false;
+        state.isActivityTypeModal = false;
+      }
+    },
+    toggleIsActivityTypeModal: (state) => {
+      state.isActivityTypeModal = !state.isActivityTypeModal;
+    },
+    closeIsActivityTypeModal: (state) => {
+      state.isActivityTypeModal = false;
+    },
+    toggleIsToolModal: (state) => {
+      state.isToolModal = !state.isToolModal;
+    },
+    closeIsToolModal: (state) => {
+      state.isToolModal = false;
+    },
+    changeStartYear: (state, action: { payload: number }) => {
+      state.modalData.Date.startYear = action.payload;
+    },
+    changeStartMonth: (state, action: { payload: number }) => {
+      state.modalData.Date.startMonth = action.payload;
+    },
+    changeEndYear: (state, action: { payload: number }) => {
+      state.modalData.Date.endYear = action.payload;
+    },
+    changeEndMonth: (state, action: { payload: number }) => {
+      state.modalData.Date.endMonth = action.payload;
     },
   },
   extraReducers: (builder) => {
@@ -35,7 +83,19 @@ export const roadMapSlice = createSlice({
   },
 });
 
-export const { toggleOrderEdit, addRoad, toggleIsModal } = roadMapSlice.actions;
+export const {
+  toggleOrderEdit,
+  addRoad,
+  toggleIsModal,
+  toggleIsToolModal,
+  closeIsToolModal,
+  changeStartYear,
+  changeStartMonth,
+  changeEndYear,
+  changeEndMonth,
+  toggleIsActivityTypeModal,
+  closeIsActivityTypeModal,
+} = roadMapSlice.actions;
 export const roadMap = (state: RootState) => state.roadMap;
 
 export default roadMapSlice.reducer;
