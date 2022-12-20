0a2ae55828b686e0a51c7bb973504b85fa9e4510
Yongyeon Kim <yeonlotus@ewhain.net>
2022-12-20 16:54:12 +0900

    Feat: 회원가입 폼 UI 구현

--- src/components/Modal/Login/index.tsx
+++ src/components/Modal/Login/index.tsx
@@ -5,12 +5,22 @@ import { IoCloseSharp } from 'react-icons/io5';
 import { Background, Modal, LoginForm, Logo } from './style';
 
 export default function LoginModal() {
+  const [isSignIn, setIsSignIn] = useState<boolean>(true);
+  const [name, setName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setaPassword] = useState<string>('');
+  const [passwordCheck, setPasswordCheck] = useState<string>('');
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // 사용자 로그인 진행 (JWT 로그인)
+    if (isSignIn) {
+      // 사용자 로그인 진행
+    } else {
+      // 회원 가입 진행
+    }
+  };
+  const changeName = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.currentTarget.value);
   };
 
   const changeEmail = (event: ChangeEvent<HTMLInputElement>) => {
@@ -19,6 +29,18 @@ export default function LoginModal() {
 
   const changePassword = (event: ChangeEvent<HTMLInputElement>) => {
     setaPassword(event.currentTarget.value);
+  };
+
+  const changePasswordCheck = (event: ChangeEvent<HTMLInputElement>) => {
+    setPasswordCheck(event.currentTarget.value);
+  };
+
+  const clickSignUp = () => {
+    setIsSignIn(false);
+  };
+
+  const clickSignIn = () => {
+    setIsSignIn(true);
   };
 
   return (
@@ -34,15 +56,29 @@ export default function LoginModal() {
         </div>
 
         <div className="field--login">
-          <LoginForm onSubmit={onSubmit}>
-            <input placeholder="이메일" value={email} onChange={changeEmail} />
-            <input placeholder="비밀번호" value={password} onChange={changePassword} />
-            <button>로그인</button>
-            <div>
-              <span>비밀번호 찾기</span> | <span>회원가입</span>
-            </div>
-          </LoginForm>
-          <div className="text">Sign in</div>
+          {isSignIn ? (
+            <LoginForm onSubmit={onSubmit}>
+              <input placeholder="이메일" value={email} onChange={changeEmail} />
+              <input placeholder="비밀번호" value={password} onChange={changePassword} />
+              <button>로그인</button>
+              <div>
+                <span>비밀번호 찾기</span> | <span onClick={clickSignUp}>회원가입</span>
+              </div>
+            </LoginForm>
+          ) : (
+            <LoginForm onSubmit={onSubmit}>
+              <input placeholder="이름" value={name} onChange={changeName} />
+              <input placeholder="이메일" value={email} onChange={changeEmail} />
+              <input placeholder="비밀번호" value={password} onChange={changePassword} />
+              <input placeholder="비밀번호 확인" value={passwordCheck} onChange={changePasswordCheck} />
+              <button>회원가입</button>
+              <div>
+                <span onClick={clickSignIn}>로그인하기</span>
+              </div>
+            </LoginForm>
+          )}
+
+          <div className="text">{isSignIn ? 'Sign in' : 'Sign up'}</div>
           <Link style={{ position: 'absolute', top: '3rem', right: '3rem' }} to="/">
             <IoCloseSharp size={24} color="white" />
           </Link>
