3b8a7236e55f0d8dc26a58a1f18223103570edc1
5hyun <qhslsl@gmail.com>
2023-01-16 22:12:55 +0900

    Feat: 활동 등록 헤더 부분 등록, 조회 기능 구현

--- src/components/Modal/ActivityInputBodys/ActivityInputHeader/index.tsx
+++ src/components/Modal/ActivityInputBodys/ActivityInputHeader/index.tsx
@@ -3,42 +3,54 @@ import { Header, TypeBtn } from './styles';
 import { useAppDispatch, useAppSelector } from '../../../../redux/hooks';
 import { toggleIsActivityTypeModal, changeTitle } from '../../../../redux/reducers/RoadMapSlice';
 import ActivityTypeModal from '../ActivityTypeModal';
+import { UploadFile } from 'antd';
 
 const ActivityInputHeader = () => {
   const dispatch = useAppDispatch();
-  const { isActivityTypeModal, isCertificate, isClub, isContest, isExternalActivity, isStudy, isEtc, title } =
-    useAppSelector((state) => state.roadMap);
-
-  const [nowType, setNowType] = useState('');
-  const types = [isCertificate, isClub, isContest, isExternalActivity, isStudy, isEtc];
-  const imgs = ['certificate', 'club', 'contest', 'external-activity', 'study', 'etc'];
+  const {
+    isActivityTypeModal,
+    isCertificate,
+    isClub,
+    isContest,
+    isExternalActivity,
+    isStudy,
+    isEtc,
+    title,
+    isEditMode,
+    itemInfo,
+    nowType,
+  } = useAppSelector((state) => state.roadMap);
 
   const onChangeTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     dispatch(changeTitle(e.target.value));
   }, []);
 
-  const onClickHeaderBtn = useCallback(() => {
+  const onClickTypeBtn = useCallback(() => {
     dispatch(toggleIsActivityTypeModal());
   }, []);
 
-  const stopPropagation = useCallback((e: any) => {
+  const stopPropagation = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
   }, []);
 
   useEffect(() => {
-    types.forEach((type, idx) => {
-      if (type) setNowType(imgs[idx]);
-    });
-
     dispatch(changeTitle(''));
   }, [isCertificate, isClub, isContest, isExternalActivity, isStudy, isEtc]);
+
+  useEffect(() => {
+    if (itemInfo) {
+      dispatch(changeTitle(itemInfo.title));
+    }
+  }, [itemInfo]);
 
   return (
     <Header>
       <TypeBtn
         onClick={(e) => {
-          stopPropagation(e);
-          onClickHeaderBtn();
+          if (isEditMode) {
+            stopPropagation(e);
+            onClickTypeBtn();
+          }
         }}
       >
         <img src={`/images/${nowType}.png`} alt={nowType} />
@@ -50,6 +62,7 @@ const ActivityInputHeader = () => {
         onChange={onChangeTitle}
         spellCheck={false}
         placeholder={'제목을 입력하세요'}
+        disabled={!isEditMode}
         required
       />
       {isActivityTypeModal && <ActivityTypeModal />}
