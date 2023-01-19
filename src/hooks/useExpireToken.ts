940b91713cac947d7cb06471b71e80d9d298537a
Yongyeon Kim <yeonlotus@ewhain.net>
2023-01-19 23:11:13 +0900

    Feat: 토큰이 만료되면 알림 추가

--- src/hooks/useExpireToken.ts
+++ src/hooks/useExpireToken.ts
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function useExpireToken() {
+  const history = useHistory();
+
   useEffect(() => {
     const accessToken = JSON.parse(localStorage.getItem('accessToken') as string);
 
@@ -9,7 +12,14 @@ export default function useExpireToken() {
 
       if (isExpire) {
         localStorage.removeItem('accessToken');
+        alert('로그인이 만료되었습니다. 다시 로그인 해주세요.');
+        refreshPage();
       }
     }
   }, []);
+
+  const refreshPage = () => {
+    history.push('/');
+    history.go(0);
+  };
 }
