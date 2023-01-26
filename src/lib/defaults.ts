6c5135c8d8de991b32f6817435b41e1da95f3872
Yongyeon Kim <yeonlotus@ewhain.net>
2023-01-26 11:23:12 +0900

    Chore: axios bsaeURL 변경

--- src/lib/defaults.ts
+++ src/lib/defaults.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASEURL = 'https://careerup.life/api';
+export const BASEURL = 'https://careerup.life:8080';
 
 export const instance = axios.create({
   baseURL: BASEURL,
